df2dfc97cd4b9b4b8e1ef2c8de12e2deed0bbc4a
prince <pansuriyaprince0@gmail.com>
2025-10-04 11:01:39 +0530

    Add files via upload

---
+++ customer.py
@@ -0,0 +1,8 @@
+#class store customer info
+class Customer:
+    
+    #cunstructor method to intialize name and moblie name
+    def __init__(self,name,mobile):
+        self.name = name #e.g,name
+        self.mobile = mobile #e.g,mobile
+        
---
+++ menu.py
@@ -0,0 +1,9 @@
+menu = {
+    "tea" : 10,
+    "coffee" : 15,
+    "sandwich" : 50,
+    "burger" : 60,
+    "pizza" : 120,
+    "room stay (1 night)" : 1000 
+}
+
